2dacdfb819c30bad8b0ef3cdeb2c062ce2c7c380
Henri Hebeisen <henri.hebeisen@gmail.com>
2022-12-14 18:27:56 +0100

    outline is now exported

--- OP_point_selection.py
+++ OP_point_selection.py
@@ -5,7 +5,14 @@ from bpy.props import BoolProperty, IntProperty
 from bpy_extras.object_utils import world_to_camera_view
 from mathutils import Vector
 
-def get_points_on_screen(context, gp_obj):
+def move_obj_to_collection(context, target_collection_name, obj):
+    for coll in obj.users_collection:
+        # Unlink the object
+        coll.objects.unlink(obj)
+    bpy.data.collections[target_collection_name].objects.link(obj)
+
+
+def delete_non_visible_points(context, gp_obj):
     start_time = time.time()
     # For each point on the stroke we check if it is visible from screen
     bpy.ops.object.mode_set(mode='EDIT_GPENCIL', toggle=False)
@@ -47,6 +54,13 @@ class GP_OT_get_points_visible(Operator):
         default=True
     )
 
+    generate_outline : BoolProperty(
+        name="Generate Outline",
+        description="Generate outline on an another layer",
+        default=True
+
+    )
+
     flatten_object : BoolProperty(
         name = "Flatten Object",
         description = "Call Reproject Operator after stroke selection",
@@ -59,31 +73,56 @@ class GP_OT_get_points_visible(Operator):
         default = 2
     )
 
+    hide_originals : BoolProperty(
+        name= "Hide Base Objects",
+        description= " Hide the mesh collection and the orginal grease pencil object after the operation",
+        default= True
+    )
+
     @classmethod
     def poll(cls, context):
-        return context.mode == 'OBJECT' and context.active_object is not None and context.active_object.type == 'GPENCIL'
+        return context.mode == 'OBJECT'
 
     def execute(self, context):
-        if self.bake_animation:
+        objects_to_flatten = []
+        if context.scene.flattener_gp_object is not None: 
+            context.view_layer.objects.active = context.scene.flattener_gp_object
+            if self.bake_animation:
+                bpy.ops.gpencil.bake_grease_pencil_animation(step=self.animation_step) #This will create a new object with baked animation
+            else:
+                gp_obj = context.active_object.copy()
+                context.view_layer.objects.link(gp_obj)
+                context.view_layer.objects.active = gp_obj
+
+            gp_obj = context.active_object
+            gp_obj.name = f"{context.scene.flattener_gp_object.name}_flattened"
+            move_obj_to_collection(context, 'Target', gp_obj)
+
+            objects_to_flatten.append(gp_obj)
+            delete_non_visible_points(context, gp_obj)
+
+        if context.scene.flattener_gp_line_art is not None:
+            context.scene.flattener_gp_line_art.select_set(True)
+            context.view_layer.objects.active = context.scene.flattener_gp_line_art
             bpy.ops.gpencil.bake_grease_pencil_animation(step=self.animation_step) #This will create a new object with baked animation
-        else:
-            new_obj = context.active_object.copy()
-            context.view_layer.objects.link(new_obj)
-            context.active_object = new_obj
-
-        gp_obj = context.active_object
-
-        get_points_on_screen(context, gp_obj)
+            line_art_obj = context.active_object
+            line_art_obj.name = f"{context.scene.flattener_gp_line_art.name}_flattened"
+            move_obj_to_collection(context, 'Target', line_art_obj)
+            objects_to_flatten.append(line_art_obj)
 
         if self.flatten_object:
             #TODO Do not depend on external operators for this one
-            #TODO Let the use choose the distance to camera for reprojection
+            #TODO Let the user choose the distance to camera for reprojection
             if context.space_data.region_3d.view_perspective != 'CAMERA':
                 bpy.ops.view3d.view_camera()
-            bpy.ops.gp.batch_reproject_all_frames()
 
+            for obj in objects_to_flatten:
+                context.view_layer.objects.active  = obj
+                bpy.ops.gp.batch_reproject_all_frames()
 
-
+        if self.hide_originals:
+            context.scene.flattener_mesh_collection.hide_viewport = True
+            context.scene.flattener_gp_object.hide_viewport = True
         return {'FINISHED'}
         
 ### Registration
