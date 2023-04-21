847cfbefa2a2451f78cfa0f4ecaf08269848daa7
Henri Hebeisen <Maxiriton@users.noreply.github.com>
2023-04-21 17:59:06 +0200

    new operator thickness

---
+++ OP_tickness_from depth.py
@@ -0,0 +1,15 @@
+import bpy
+
+
+### Registration
+classes = (
+GP_OT_change_tickness,
+)
+
+def register():
+    for cl in classes:  
+        bpy.utils.register_class(cl)
+
+def unregister():
+    for cl in reversed(classes):
+        bpy.utils.unregister_class(cl)
